8c513a07edc7d47151e55d368015ff65cba1b1ad
ZoyKad <77884733+ZoyKad@users.noreply.github.com>
2023-10-22 23:55:26 +0200

    Refactored setup

--- src/main.cpp
+++ src/main.cpp
@@ -40,61 +40,81 @@ void setup() {
 
   Serial.println("Setup in progress...");
 
-  // Setup display
+ auto setupDisplay = []() {
+    const uint8_t middleBar[1] = {SEG_G};
+    scoreDisplay.setBrightness(0x0f);
+    scoreDisplay.setSegments(middleBar, 4, 0);
+  };
 
-  const uint8_t middleBar[1] = {SEG_G};
-  scoreDisplay.setBrightness(0x0f);
-  scoreDisplay.setSegments(middleBar, 4, 0);
+  auto setupRadio = []() {
+    radio.begin();
 
+    radio.setChannel(99);
+    radio.setDataRate(RF24_250KBPS);
+    radio.setPALevel(RF24_PA_LOW);
+    radio.openReadingPipe(0, readingAddress); // address
+    radio.openWritingPipe(writingAddress); // address
+  };
+
+  auto connectToControllerAsync = []() {
+    const unsigned char connectionMessage = '!';
+
+    while (true) {
+      radio.stopListening();
+
+      radio.write(&connectionMessage, sizeof(connectionMessage));
+
+      radio.startListening();
+
+      delay(1000);
+
+      while (radio.available()) {
+        unsigned char data;
+
+        radio.read(&data, sizeof(data));
+        
+        if (data != '!') continue;
+
+        return;
+      };
+    }
+  };
+
+  auto setupComponents = []() {
+    for (unsigned char i = 0; i < 3; ++i) {
+      pinMode(electroMagnetPins[i], OUTPUT);
+    }
+
+    horizontalServo.attach(10); // Pin
+    horizontalServo.write(0);
+
+    leftGrabberServo.attach(11); // Pin
+    leftGrabberServo.write(0);
+
+    rightGrabberServo.attach(12); // Pin
+    rightGrabberServo.write(0);
+  };
+
+  Serial.begin(9600);
+
+  Serial.println("Setup in progress...");
+
+  setupDisplay();
   Serial.println("Display setup completed");
 
-  // Setup servos
-
-  horizontalServo.attach(10); // Pin
-  horizontalServo.write(0);
-
-  leftGrabberServo.attach(11); // Pin
-  leftGrabberServo.write(0);
-
-  rightGrabberServo.attach(12); // Pin
-  rightGrabberServo.write(0);
-
-  Serial.println("Motors setup completed");
-
-  // Setup radio
-
-  radio.begin();
-
-  radio.setChannel(99);
-  radio.setDataRate(RF24_250KBPS);
-  radio.setPALevel(RF24_PA_LOW);
-  radio.openReadingPipe(0, readingAddress); // address
-  radio.openWritingPipe(writingAddress); // address
-
+  setupRadio();
   Serial.println("Radio setup completed");
 
-  Serial.println("Initialization complete!");
+  setupComponents();
+  Serial.println("Components setup completed");
 
-  // Waiting for controller to connect
+  Serial.println("Initialization completed!");
 
-  Serial.println("Waiting for controller to connect...");
+  Serial.println("Attempting to connect to robot...");
+  connectToControllerAsync();
+  Serial.println("Controller connected!");
 
-  radio.startListening();
-
-  while (true) {
-    delay(1000);
-    if (radio.available()) {
-      unsigned char data;
-
-      radio.read(&data, sizeof(data));
-
-      if (data == '!') {
-        Serial.println("Controller connected!");
-        scoreDisplay.showNumberDec(DEFAULT_SCORE, false, 4, 0);
-        break;
-      }
-    }
-  }
+  scoreDisplay.showNumberDec(DEFAULT_SCORE, false, 4, 0);
 
   Serial.println("Robot ready!");
 }
@@ -118,7 +138,7 @@ void loop() {
       joystickData.omniX = data[2];
       joystickData.omniY = data[3];
 
-
+      // TODO: Implement joystick data
     } else if (dataType == 'S') {
       // Score data
       
