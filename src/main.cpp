d4d361b5d25bc1671f57906e70cc5a41f29b9267
ZoyKad <77884733+ZoyKad@users.noreply.github.com>
2023-10-22 11:56:24 +0200

    Improved radio connection logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,24 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include <RF24.h>
+#include <TM1637Display.h>
 
-const uint64_t readingAddress = 314592654UL;
+#define DEFAULT_SCORE 50
+
+const uint64_t readingAddress = 2024134UL;
 const uint64_t writingAddress = 314592654UL;
 
+TM1637Display display(12, 13); // CLK, DIO
 RF24 radio(9, 53); // CE, CSN
+
+struct JOYSTICK_DATA {
+  // First joystick
+  unsigned short x;
+  unsigned short y;
+
+  // Second joystick
+  unsigned short omniX;
+};
 
 void setup() {
   // Setup Serial
@@ -13,6 +26,9 @@ void setup() {
   Serial.begin(9600);
 
   Serial.println("Setup in progress...");
+
+  const uint8_t middleBar[1] = {SEG_G};
+  display.setSegments(middleBar, 4, 0);
 
   // Setup radio
 
@@ -37,8 +53,15 @@ void setup() {
   while (true) {
     delay(1000);
     if (radio.available()) {
-      Serial.println("Controller connected!");
-      break;
+      unsigned char data[2];
+
+      radio.read(&data, sizeof(data));
+
+      if (data[1] == 255 && data[2] == 255) {
+        Serial.println("Controller connected!");
+        display.showNumberDec(DEFAULT_SCORE, false, 4, 0);
+        break;
+      }
     }
   }
 
@@ -46,5 +69,20 @@ void setup() {
 }
 
 void loop() {
-  
+  if (radio.available()) {
+    unsigned char data[4];
+
+    radio.read(&data, sizeof(data));
+
+    if (data[1] == 'J') {
+      JOYSTICK_DATA joystickData;
+
+      joystickData.x = short(data[2]);
+      joystickData.y = short(data[3]);
+      joystickData.omniX = short(data[4]);
+
+    } else if (data[1] == 'S') {
+      short score = short(data[2]);
+    }
+  }
 }
