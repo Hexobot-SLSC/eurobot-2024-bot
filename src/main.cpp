7abe3fed53957e1e616967d238d3eb81ae1ea7a5
ZoyKad <maxcaron@outlook.com>
2023-10-22 17:29:14 +0000

    Added servos control

--- src/main.cpp
+++ src/main.cpp
@@ -2,22 +2,35 @@
 #include <SPI.h>
 #include <RF24.h>
 #include <TM1637Display.h>
+#include <Servo.h>
 
 #define DEFAULT_SCORE 50
 
 const uint64_t readingAddress = 2024134UL;
 const uint64_t writingAddress = 314592654UL;
 
-TM1637Display display(12, 13); // CLK, DIO
+const unsigned char electroMagnetPins[3] = {
+  6,
+  7,
+  8
+};
+
+TM1637Display scoreDisplay(12, 13); // CLK, DIO
+
 RF24 radio(9, 53); // CE, CSN
+
+Servo horizontalServo;
+Servo leftGrabberServo;
+Servo rightGrabberServo;
 
 struct JOYSTICK_DATA {
   // First joystick
-  unsigned short x;
-  unsigned short y;
+  char x;
+  char y;
 
   // Second joystick
-  unsigned short omniX;
+  char omniX;
+  char omniY;
 };
 
 void setup() {
@@ -27,8 +40,24 @@ void setup() {
 
   Serial.println("Setup in progress...");
 
+  // Setup display
+
   const uint8_t middleBar[1] = {SEG_G};
-  display.setSegments(middleBar, 4, 0);
+  scoreDisplay.setBrightness(0x0f);
+  scoreDisplay.setSegments(middleBar, 4, 0);
+
+  Serial.println("Display setup completed");
+
+  // Setup servos
+
+  horizontalServo.attach(10); // Pin
+  horizontalServo.write(0);
+
+  leftGrabberServo.attach(11); // Pin
+  leftGrabberServo.write(0);
+
+  rightGrabberServo.attach(12); // Pin
+  rightGrabberServo.write(0);
 
   // Setup radio
 
@@ -40,9 +69,9 @@ void setup() {
   radio.openReadingPipe(0, readingAddress); // address
   radio.openWritingPipe(writingAddress); // address
 
-  Serial.println("Radio started");
+  Serial.println("Radio setup completed");
 
-  Serial.println("Setup complete!");
+  Serial.println("Initialization complete!");
 
   // Waiting for controller to connect
 
@@ -53,36 +82,80 @@ void setup() {
   while (true) {
     delay(1000);
     if (radio.available()) {
-      unsigned char data[2];
+      unsigned char data;
 
       radio.read(&data, sizeof(data));
 
-      if (data[1] == 255 && data[2] == 255) {
+      if (data == '!') {
         Serial.println("Controller connected!");
-        display.showNumberDec(DEFAULT_SCORE, false, 4, 0);
+        scoreDisplay.showNumberDec(DEFAULT_SCORE, false, 4, 0);
         break;
       }
     }
   }
 
-  Serial.println("Bot ready!");
+  Serial.println("Robot ready!");
 }
 
 void loop() {
   if (radio.available()) {
-    unsigned char data[4];
+    char dataType;
 
-    radio.read(&data, sizeof(data));
+    radio.read(&dataType, sizeof(dataType));
 
-    if (data[1] == 'J') {
+    if (dataType == 'J') {
+      // Joystick data
+
+      short data[4];
       JOYSTICK_DATA joystickData;
 
-      joystickData.x = short(data[2]);
-      joystickData.y = short(data[3]);
-      joystickData.omniX = short(data[4]);
+      radio.read(&data, sizeof(data));
 
-    } else if (data[1] == 'S') {
-      short score = short(data[2]);
+      joystickData.x = data[1];
+      joystickData.y = data[2];
+      joystickData.omniX = data[3];
+      joystickData.omniY = data[4];
+
+    } else if (dataType == 'S') {
+      // Score data
+      
+      char score;
+
+      radio.read(&score, sizeof(score));
+
+      scoreDisplay.showNumberDec(score);
+    } else if (dataType == 'H') {
+      // Horizontal servo data
+
+      unsigned char servoAngle;
+
+      radio.read(&servoAngle, sizeof(servoAngle));
+    } else if (dataType == 'L') {
+      // Left grabber servo data
+
+      unsigned char servoAngle;
+
+      radio.read(&servoAngle, sizeof(servoAngle));
+
+      leftGrabberServo.write(servoAngle);
+    } else if (dataType == 'R') {
+      // Right grabber servo data
+
+      unsigned char servoAngle;
+
+      radio.read(&servoAngle, sizeof(servoAngle));
+
+      rightGrabberServo.write(servoAngle);
+    } else if (dataType == 'E') {
+      // Electro magnet data
+
+      unsigned char areMagnetEnabled;
+
+      radio.read(&areMagnetEnabled, sizeof(areMagnetEnabled));
+
+      for (unsigned char i = 0; i < 3; ++i) {
+        digitalWrite(electroMagnetPins[i], areMagnetEnabled);
+      }
     }
   }
 }
