1e375728b005aef220673a1902474269bd05fcbb
ZoyKad <77884733+ZoyKad@users.noreply.github.com>
2024-01-26 23:04:15 +0100

    Add test mode and serial output format improves

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,50 @@
 #include <Servo.h>
 #include <SabertoothSimplified.h>
 #include <SoftwareSerial.h>
+#include <printf.h>
 
-#define DEFAULT_SCORE 50
+// USER DEFINED //
+
+/* Define wether to enable info logs or not. Add // before #define to disable. */
+#define INFO_STATE
+
+/* Define wether to enable debug logs or not. Add // before #define to disable. */
+#define DEBUG_STATE
+
+/* Enable or disable test sequence. /!\ MUST BE DISABLED BEFORE NORMAL USAGE /!\ */
+#define TEST_MODE
+
+// END USER DEFINED //
+
+#ifdef INFO_STATE
+#define log(msg) \
+Serial.print("[INFO] : "); \
+Serial.println(msg);
+#else
+#define log(msg)
+#endif
+
+#ifdef DEBUG_STATE
+#define debug(msg) \
+Serial.print("[DEBUG] : "); \
+Serial.println(msg);
+#else
+#define debug(msg)
+#endif
+
+#define DEFAULT_SCORE 40
 #define ELECTRO_MAGNETS_COUNT 3
 
-const byte electroMagnetPins[ELECTRO_MAGNETS_COUNT] = {2, 3, 4};
+const byte electroMagnetPins[ELECTRO_MAGNETS_COUNT] = {3, 4, 5};
 
 TM1637Display scoreDisplay(12, 13); // CLK, DIO
 
 RF24 radio(9, 53); // CE, CSN
 
-SoftwareSerial sabertoothRightSerial(NOT_A_PIN, 18); // RX, TX
+SoftwareSerial sabertoothRightSerial(NOT_A_PIN, 16); // RX, TX
+SoftwareSerial sabertoothLeftSerial(NOT_A_PIN, 18); // RX, TX
 SabertoothSimplified sabertoothRight(sabertoothRightSerial);
-SabertoothSimplified sabertoothLeft;
+SabertoothSimplified sabertoothLeft(sabertoothLeftSerial);
 
 Servo grabberHeightServo;
 Servo grabberOpeningServo;
@@ -68,13 +99,15 @@ void getData(RadioData *dataBuffer);
 void applyDataUpdates(RadioData *newData);
 void updateMotors(RadioData *newData);
 bool areJoysticksDifferent(JoystickData *first, JoystickData *second);
+void testLoop();
 
 void setup() {
   // Setup Serial
 
   Serial.begin(9600);
-
-  Serial.println("Setup in progress...");
+  printf_begin();
+  sabertoothLeftSerial.begin(9600);
+  sabertoothRightSerial.begin(9600);
 
  auto setupScoreDisplay = []() {
     const uint8_t middleBar[1] = {SEG_G};
@@ -102,31 +135,176 @@ void setup() {
     grabberOpeningServo.attach(11); // Pin
     grabberOpeningServo.write(0);
 
-    pushersServos[0].attach(12); // Pin
+    pushersServos[0].attach(6); // Pin
     pushersServos[0].write(0);
 
-    pushersServos[1].attach(13); // Pin
+    pushersServos[1].attach(7); // Pin
     pushersServos[1].write(0);
 
     for (byte i = 0; i < ELECTRO_MAGNETS_COUNT; ++i) digitalWrite(electroMagnetPins[i], LOW);
   };
 
   Serial.begin(9600);
+  printf_begin();
 
-  Serial.println("Setup in progress...");
+  log("Setup in progress...");
 
   setupScoreDisplay();
-  Serial.println("Display setup completed");
+  debug("Display setup completed");
   
   setupComponents();
-  Serial.println("Components setup completed");
+  debug("Components setup completed");
 
   setupRadio();
-  Serial.println("Radio setup completed");
+  debug("Radio setup completed");
 
   radio.flush_rx();
 
-  Serial.println("Robot ready!");
+  log("Robot ready!");
+
+  #ifdef DEBUG_STATE
+  radio.printDetails();
+  #endif
+
+  #ifdef TEST_MODE
+  while(true) testLoop();
+  #endif
+}
+
+void testLoop() {
+  log("Test started!");
+
+  // for (char i = -127; i < 127; ++i) {
+  //   sabertoothLeft.motor(1, i);
+  //   delay(10);
+  // }
+  // for (char i = -127; i < 127; ++i) {
+  //   sabertoothLeft.motor(2, i);
+  //   delay(40);
+  // }
+  // for (char i = -127; i < 127; ++i) {
+  //   sabertoothRight.motor(1, i);
+  //   delay(40);
+  // }
+  // for (char i = -127; i < 127; ++i) {
+  //   sabertoothRight.motor(2, i);
+  //   delay(40);
+  // }
+
+  debug("Going forward");
+
+  sabertoothLeft.motor(1, 40);
+  sabertoothLeft.motor(2, 40);
+  sabertoothRight.motor(1, 40);
+  sabertoothRight.motor(2, 40);
+
+  delay(2500);
+
+  debug("Stop");
+
+  sabertoothLeft.stop();
+  sabertoothRight.stop();
+
+  delay(2000);
+
+  debug("Going backwards");
+
+  sabertoothLeft.motor(1, -40);
+  sabertoothLeft.motor(2, -40);
+  sabertoothRight.motor(1, -40);
+  sabertoothRight.motor(2, -40);
+
+  delay(2500);
+
+  debug("Stop");
+
+  sabertoothLeft.stop();
+  sabertoothRight.stop();
+
+  delay(2000);
+
+  debug("Turn left");
+
+  sabertoothLeft.motor(1, -40);
+  sabertoothLeft.motor(2, -40);
+  sabertoothRight.motor(1, 40);
+  sabertoothRight.motor(2, 40);
+
+  delay(2500);
+
+  debug("Stop");
+
+  sabertoothLeft.stop();
+  sabertoothRight.stop();
+
+  delay(2000);
+
+  debug("Turn right");
+
+  sabertoothLeft.motor(1, 40);
+  sabertoothLeft.motor(2, 40);
+  sabertoothRight.motor(1, -40);
+  sabertoothRight.motor(2, -40);
+
+  delay(2500);
+
+  debug("Stop")
+
+  sabertoothLeft.stop();
+  sabertoothRight.stop();
+
+  delay(2000);
+
+  debug("Pan right");
+
+  sabertoothLeft.motor(1, -90);
+  sabertoothLeft.motor(2, 90);
+  sabertoothRight.motor(1, -90);
+  sabertoothRight.motor(2, 90);
+
+  delay(2500);
+  
+  debug("Pan left");
+
+  sabertoothLeft.motor(1, 40);
+  sabertoothLeft.motor(2, -40);
+  sabertoothRight.motor(1, 40);
+  sabertoothRight.motor(2, -40);
+
+  delay(2500);
+
+  debug("Stop");
+
+  debug("Stop");
+
+  delay(8000);
+
+  log("Test ended");
+
+  // sabertoothLeft.stop();
+  // sabertoothRight.stop();
+
+  // pushersServos[0].write(0);
+  // pushersServos[1].write(180);
+
+  // delay(1000);
+
+  // pushersServos[0].write(180);
+  // pushersServos[1].write(0);
+
+  // // for (int i = 100; i > 0; --i) {
+  // //   sabertoothLeft.motor(0, i);
+  // //   sabertoothLeft.motor(1, i);
+  // //   sabertoothRight.motor(0, i);
+  // //   sabertoothRight.motor(1, i);
+  // //   delay(40);
+  // // }
+
+  // sabertoothLeft.stop();
+  // sabertoothRight.stop();
+
+  // Serial.println("Test completed!");
+  // delay(5000);
 }
 
 void loop() {
@@ -138,6 +316,8 @@ void loop() {
 
   getData(&receivedData);
   applyDataUpdates(&receivedData);
+
+  Serial.println(receivedData.areMagnetsEnabled);
 
   lastReceivedData = receivedData;
 }
@@ -192,7 +372,7 @@ void updateMotors(RadioData *newData) {
   short thetaSin = sin(theta - PI / 4);
   short maxValue = max(abs(thetaCos), abs(thetaSin));
 
-  short turnValue = map(newData->joystickData.x, 0, 255, -25, 25);
+  short turnValue = map(newData->joystickData.x, 0, 255, -40, 40);
 
   sabertoothLeft.motor(0, thetaCos / maxValue * 100 + turnValue);
   sabertoothLeft.motor(1, thetaSin / maxValue * 100 - turnValue); 
